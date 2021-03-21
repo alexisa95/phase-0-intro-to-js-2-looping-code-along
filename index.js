68c06323e9e6ea25ccdbc2420e686a07642771fd
Alexis <lexisadams92@gmail.com>
2021-03-21 16:10:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards (names, eventName) {
+  const response = [];
+  for (let i = 0; i<names.length; i++) {
+    response.push (`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return response;
+}
+
+function countDown (num) {
+  for (let i=num; i >=0; i--) {
+    console.log (i);
+  }
+}
